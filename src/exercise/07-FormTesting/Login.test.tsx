ecd2c821c15e4b74cf77b725dd679d81b8a2c9fd
ArturoRM22 <A01643269@tec.mx>
2025-04-11 18:02:24 -0600

    New tests exercises

---
+++ src/exercise/07-FormTesting/Login.test.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { render, fireEvent, screen, getByRole } from "@testing-library/react";
+import "@testing-library/jest-dom"; // Add this import
+import Login from "../sharedComponent/Login";
+import userEvent from "@testing-library/user-event";
+
+describe('Render login', () => {
+ test('handles login form inputs and onSubmit button', () => {
+    const handleSubmit = jest.fn();
+
+    render(<Login onSubmit={handleSubmit} />);
+
+    fireEvent.change(screen.getByLabelText(/username/i), { 
+        target: { value: 'testuser' } 
+    });
+
+    fireEvent.change(screen.getByLabelText(/password/i), { 
+        target: { value: 'password123' } 
+      });
+
+      fireEvent.click(screen.getByRole('button', { name: /submit/i }));
+        // Verify submission
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username: 'testuser',
+    password: 'password123'
+  });
+});
+});
